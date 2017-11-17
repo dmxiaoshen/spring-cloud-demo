e0daadd412b7f4686b67300bac9f84731ab4e8e3
hushengen <hzhsg@tairanchina.com>
2017-11-17 11:15:49 +0800

    fegin date param

--- spring-cloud-eureka-client/src/main/java/com/dmxiaoshen/SpringCloudEurekaClientApplication.java
+++ spring-cloud-eureka-client/src/main/java/com/dmxiaoshen/SpringCloudEurekaClientApplication.java
@@ -1,8 +1,19 @@
 package com.dmxiaoshen;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.support.GenericConversionService;
+import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+import javax.annotation.PostConstruct;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,5 +21,43 @@ public class SpringCloudEurekaClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudEurekaClientApplication.class, args);
+	}
+
+
+	@Configuration
+	public class WebConfigBeans {
+		@Autowired
+		private RequestMappingHandlerAdapter handlerAdapter;
+
+		/**
+		 * 增加字符串转日期的功能
+		 */
+		@PostConstruct
+		public void initEditableValidation() {
+			ConfigurableWebBindingInitializer initializer = (ConfigurableWebBindingInitializer) handlerAdapter
+					.getWebBindingInitializer();
+			if (initializer.getConversionService() != null) {
+				GenericConversionService genericConversionService = (GenericConversionService) initializer
+						.getConversionService();
+				genericConversionService.addConverter(String.class, Date.class, new String2DateConverter());
+			}
+		}
+
+		/**
+		 * 字符串转化成日期的适配，默认是Fri Nov 11 00:00:00 CST 1222 这种类型
+		 * 这里填写需要支持的几种都行
+		 */
+		private class String2DateConverter implements Converter<String, Date> {
+			@Override
+			public Date convert(String source) {
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				try {
+					return sdf.parse(source);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				return null;
+			}
+		}
 	}
 }
--- spring-cloud-eureka-consumer-feign/src/main/java/com/dmxiaoshen/SpringCloudEurekaConsumerFeignApplication.java
+++ spring-cloud-eureka-consumer-feign/src/main/java/com/dmxiaoshen/SpringCloudEurekaConsumerFeignApplication.java
@@ -4,13 +4,45 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignFormatterRegistrar;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 public class SpringCloudEurekaConsumerFeignApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudEurekaConsumerFeignApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaConsumerFeignApplication.class, args);
+    }
+
+    /**
+     * fegin 调用传日期参数是默认 Fri Nov 11 00:00:00 CST 1222 这种类型
+     * 这里自定义格式化，但是需要服务方能转化这种格式
+     */
+    @Component
+    public class DateFormatRegister implements FeignFormatterRegistrar {
+        public DateFormatRegister() {
+
+        }
+
+        @Override
+        public void registerFormatters(FormatterRegistry registry) {
+            registry.addConverter(Date.class, String.class, new Date2StringConverter());
+        }
+
+        private class Date2StringConverter implements Converter<Date, String> {
+            @Override
+            public String convert(Date source) {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                return sdf.format(source);
+            }
+
+        }
+    }
 }
